8b405d39641419652f0722e9d81f1bf04a2ad392
Wilfried Mbouenda Mbogne <WillyShakes@users.noreply.github.com>
2018-04-10 22:35:19 +0200

    Update constants.dart

--- lib/Utils/constants.dart
+++ lib/Utils/constants.dart
@@ -1,5 +1,5 @@
 class constants {
-  static String APP_ID = "0gFFyIoKa22pIQqG2acOKugXC--_d-ts";
-  static String APP_SECRET = "8SSV3ytyJBIas4ai1Tj4jpBDhCU27bDzXydw2TIFSXhGnvD81-y4J6tG7E05_hGy";
-  static String APP_DOMAIN = "willyshakes.eu.auth0.com";
+  static String APP_ID = "";
+  static String APP_SECRET = "";
+  static String APP_DOMAIN = "";
 }
